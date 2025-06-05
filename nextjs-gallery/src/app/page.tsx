8df8ae3cd78a02f9a783291eba16ccc8d7624eba
Majito Brinkitos <mcvef2013@gmail.com>
2025-06-05 11:04:33 -0700

    navbar, footer, gallery

--- nextjs-gallery/src/app/page.tsx
+++ nextjs-gallery/src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
    <>
     <h1 className="text-[50px] flex justify-center">My name is Maria-Jose Mejia</h1>
-    <div className="h-145">
+    <div className="h-145 p-50">
         <h2 className="text-[30px] flex justify-center mr-5">Welcome to my gallery</h2>
     <p className="text-[20px] flex justify-center mr-5">And my todos website</p>
     </div>
