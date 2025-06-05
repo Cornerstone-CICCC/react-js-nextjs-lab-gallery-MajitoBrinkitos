5a891cc46ec5d3c627b79eec5fd1051f7941abe0
Majito Brinkitos <mcvef2013@gmail.com>
2025-06-05 09:42:23 -0700

    navbar done

--- nextjs-gallery/src/components/Navbar.tsx
+++ nextjs-gallery/src/components/Navbar.tsx
@@ -6,8 +6,8 @@ export default function Navbar() {
     <nav className="bg-gray-800 text-white flex items-center justify-between p-4">
         <img className="flex h-10" src="/logo.svg" alt="logo home page" />
         <div className='flex gap-4'>
-            <Link href="/" className="hover:underline flex">Home</Link>
-            <Link href="/gallery" className="hover:underline flex">Gallery</Link>
+            <Link href="/" className="hover:underline flex text-[24px]">Home</Link>
+            <Link href="/gallery" className="hover:underline flex text-[24px]">Gallery</Link>
         </div>
     </nav>
     </>
